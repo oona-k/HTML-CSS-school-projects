80d2eecf3d01b40506d0a681b967486f55f81ecb
oona-k <59806531+oona-k@users.noreply.github.com>
2020-03-18 13:56:18 +0200

    Added back to top button

---
+++ 16_jobs_in_finland/script.js
@@ -0,0 +1,18 @@
+// https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+var topbutton = document.getElementById("scroll-top-btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
